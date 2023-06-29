0d99feb38546402d5ba8961bb6349e6f4dee062d
Timofey Melikhov <Schibatuch@gmail.com>
2023-06-29 17:19:00 +0300

    add debounce hook

--- src/components/movieSearch/MovieSearch.tsx
+++ src/components/movieSearch/MovieSearch.tsx
@@ -2,16 +2,31 @@ import React, { useEffect } from 'react'
 import classes from './movieSearch.module.css'
 import { useInput } from '../../hooks/input'
 import { useDebounce } from '../../hooks/debounce'
+import axios from 'axios'
 
 export function MovieSearch() {
 
   const input = useInput('')
 
-  const debounced = useDebounce(input.value)
+  const debounced = useDebounce<string>(input.value, 550)
+
+  async function searchMovies() {
+    const response = await axios.get(`https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword`, 
+      {
+        headers: {
+          'X-API-KEY': '03b257a3-99b3-43ff-be90-2f7b5b72e260',
+          'Content-Type': 'application/json',
+        },
+        params: {
+          keyword: debounced,
+          page: 1
+        }
+      }
+    )
+  }
 
   useEffect(() => {
     if (debounced.length > 3) {
-
     }
   }, [debounced])
 
--- src/hooks/debounce.ts
+++ src/hooks/debounce.ts
@@ -4,9 +4,7 @@ export function useDebounce<T>(value: T, delay = 250) {
   const [debouncedValue, setDebouncedValue] = useState(value)
 
   useEffect(() => {
-    const handler = setTimeout(() => {
-      setDebouncedValue(value)
-    }, delay)
+    const handler = setTimeout(() => setDebouncedValue(value), delay)
     return () => clearTimeout(handler)
   }, [value, delay])
 
