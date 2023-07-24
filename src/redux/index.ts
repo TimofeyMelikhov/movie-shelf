87cc7021672eaf4399dc2f74bb38ad6cc8c06f0a
Timofey Melikhov <Schibatuch@gmail.com>
2023-07-24 17:46:56 +0300

    switch search to rtk query

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,14 +1,12 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import authReducer from './slices/userSlice'
 import movieDetailReducer from './slices/movieDetailSlice'
-import searchMovieReducer from './slices/searchMovieSlice'
 import personDetailReducer from './slices/personDetailSlice'
 import { movieApi } from './movies.api'
 
 const rootReducer = combineReducers({
   authReducer,
   movieDetailReducer,
-  searchMovieReducer,
   personDetailReducer,
   [movieApi.reducerPath]: movieApi.reducer
 })
