46ca8d9fe59f916820ee7ffe25cbf6b00287411f
Shibatuch <Schibatuch@gmail.com>
2023-08-06 22:33:35 +0300

    fix queryFn

--- src/pages/MovieDetailPage/MovieDetails.tsx
+++ src/pages/MovieDetailPage/MovieDetails.tsx
@@ -6,36 +6,25 @@ import { formatDate, convertMinutesToHours,} from '../../utils/formatter'
 import { RelatedFilms } from './RelatedFilms'
 import { Preloader } from '../../components/preloader/Preloader'
 import {
-  useGetMovieDetailsQuery,
-  useGetStaffMovieQuery,
-  useGetBudgetMovieQuery,
-  useGetDistributionMovieQuery,
-  useGetPrequelMoviesQuery,
-  useGetFactsMovieQuery,
-  useGetSimilarsMovieQuery
+  useGetCombineDataOnMovieQuery
 } from '../../redux/movies.api'
 
 export const MovieDetails: React.FC = () => {
 
   const {id} = useParams<'id'>()
 
-  const { data: detailMovie, isLoading } = useGetMovieDetailsQuery(id)
-  const { data: staff } = useGetStaffMovieQuery(id)
-  const { data: budget } = useGetBudgetMovieQuery(id)
-  const { data: distribution } = useGetDistributionMovieQuery(id)
-  const { data: prequelMovies } = useGetPrequelMoviesQuery(id)
-  const { data: facts } = useGetFactsMovieQuery(id)
-  const { data: similarsMovie } = useGetSimilarsMovieQuery(id)
+  const { data, isLoading } = useGetCombineDataOnMovieQuery(id)
 
   const [hasSequelsAndPrequels, setHasSequelsAndPrequels] = useState(false);
 
+
   useEffect(() => {
-    if (prequelMovies?.length) setHasSequelsAndPrequels(true)
-  }, [prequelMovies?.length])
+    if (data?.PrequelMovies.length) setHasSequelsAndPrequels(true)
+  }, [data?.PrequelMovies.length])
 
   const generateStaffLinks = (professionKey: string) => {
-    const filteredStaff = staff
-      ?.filter(item => item.professionKey === professionKey)
+    const filteredStaff = data?.staff
+      .filter(item => item.professionKey === professionKey)
       .map((item, index) => (
         item.nameRu ? 
           <React.Fragment key={item.staffId}>
@@ -50,8 +39,8 @@ export const MovieDetails: React.FC = () => {
     return filteredStaff?.filter(item => item !== null).splice(0, 3);
   };
   const generateActorLinks = (professionKey: string) => {
-    const filteredStaff = staff
-      ?.filter(item => item.professionKey === professionKey)
+    const filteredStaff = data?.staff
+      .filter(item => item.professionKey === professionKey)
       .map(item => (
         item.nameRu ? 
           <React.Fragment key={item.staffId}>
@@ -66,26 +55,26 @@ export const MovieDetails: React.FC = () => {
   };
 
   const getterBudget = (typeBudget: string) => {
-    return budget?.items.filter(item => item.type === typeBudget)
+    return data?.budget?.items.filter(item => item.type === typeBudget)
       .map(item => {
         const amount = item.amount.toLocaleString()
         return item.symbol + amount
       })
   }
 
-  const countries = detailMovie?.countries.map(c => c.country).join(', ')
-  const genres = detailMovie?.genres.map(g => g.genre).join(', ')
-  const movieTime = detailMovie?.filmLength ? `${detailMovie.filmLength} мин. / ${convertMinutesToHours(detailMovie.filmLength)}` : 'Неизвестно'
-  const slogan = detailMovie?.slogan ? `«${detailMovie.slogan}»` : '—'
-  const ratingCount = detailMovie?.ratingKinopoiskVoteCount.toLocaleString('ru-RU')
-  const ageRaiting = detailMovie?.ratingAgeLimits ? Number(detailMovie.ratingAgeLimits?.match(/\d+/)) + '+' : null
-  const highlitedRating = detailMovie?.ratingKinopoisk && detailMovie.ratingKinopoisk >= 7 ? classes.high_rating : detailMovie?.ratingKinopoisk &&
-    detailMovie.ratingKinopoisk >= 5.1 && detailMovie.ratingKinopoisk <= 6.9 ? classes.medium_rating : classes.low_rating
-    const premierInRussiaDate = distribution?.items.filter(item => (item.type === 'PREMIERE' || item.type === 'COUNTRY_SPECIFIC') &&  
+  const countries = data?.movieDetails.countries.map(c => c.country).join(', ')
+  const genres = data?.movieDetails.genres.map(g => g.genre).join(', ')
+  const movieTime = data?.movieDetails.filmLength ? `${data?.movieDetails.filmLength} мин. / ${convertMinutesToHours(data?.movieDetails.filmLength)}` : 'Неизвестно'
+  const slogan = data?.movieDetails.slogan ? `«${data?.movieDetails.slogan}»` : '—'
+  const ratingCount = data?.movieDetails.ratingKinopoiskVoteCount.toLocaleString('ru-RU')
+  const ageRaiting = data?.movieDetails.ratingAgeLimits ? Number(data?.movieDetails.ratingAgeLimits?.match(/\d+/)) + '+' : null
+  const highlitedRating = data?.movieDetails.ratingKinopoisk && data?.movieDetails.ratingKinopoisk >= 7 ? classes.high_rating : data?.movieDetails.ratingKinopoisk &&
+    data?.movieDetails.ratingKinopoisk >= 5.1 && data?.movieDetails.ratingKinopoisk <= 6.9 ? classes.medium_rating : classes.low_rating
+    const premierInRussiaDate = data?.distribution.items.filter(item => (item.type === 'PREMIERE' || item.type === 'COUNTRY_SPECIFIC') &&  
     (item.country && item.country.country === "Россия")).map(item => item.date)[0]
-  const worldPremierDate = distribution?.items.filter(item => item.type === 'WORLD_PREMIER').map(item => item.date)
-  const distributionOnBluDate = distribution?.items.filter(item => item.subType === 'BLURAY').map(item => item.date)[0]
-  const distributionOnDvdDate = distribution?.items.filter(item => item.subType === 'DVD').map(item => item.date)[0]
+  const worldPremierDate = data?.distribution.items.filter(item => item.type === 'WORLD_PREMIER').map(item => item.date)
+  const distributionOnBluDate = data?.distribution.items.filter(item => item.subType === 'BLURAY').map(item => item.date)[0]
+  const distributionOnDvdDate = data?.distribution.items.filter(item => item.subType === 'DVD').map(item => item.date)[0]
 
   const director = generateStaffLinks('DIRECTOR');
   const writer = generateStaffLinks('WRITER');
@@ -112,7 +101,7 @@ export const MovieDetails: React.FC = () => {
         <div className='container mx-auto pt-5 max-w-[1300px] bg-white '>
           <div className=' flex justify-evenly'>
             <div className='flex flex-col'>
-              <img src={detailMovie?.posterUrl} alt="poster" className='mb-[25px]'/>
+              <img src={data?.movieDetails?.posterUrl} alt="poster" className='mb-[25px]'/>
               {/* <iframe
                 width="300"
                 height="200"
@@ -123,14 +112,14 @@ export const MovieDetails: React.FC = () => {
               ></iframe> */}
             </div>
             <div className='flex flex-col ml-[40px] max-w-[55%]'>
-              <h1 className={classes.header}>{ detailMovie?.nameRu } ({detailMovie?.year})</h1>
-              <div className={classes.origName}>{detailMovie?.nameOriginal} {ageRaiting}</div>
-              <div>Рейтинг Кинопоиска: <span className={highlitedRating}>{ detailMovie?.ratingKinopoisk }</span></div>
+              <h1 className={classes.header}>{ data?.movieDetails?.nameRu } ({data?.movieDetails?.year})</h1>
+              <div className={classes.origName}>{data?.movieDetails?.nameOriginal} {ageRaiting}</div>
+              <div>Рейтинг Кинопоиска: <span className={highlitedRating}>{ data?.movieDetails?.ratingKinopoisk }</span></div>
               <div className='text-[14px]'> { ratingCount } оценок </div>
               <div className='mt-[50px] text-[22px]'>О Фильме</div>
               <div className='mt-[10px] text-[13px]'>
                 <span className='text-gray-600 inline-flex w-[180px]'> Год производства  </span>
-                <span > { detailMovie?.year } </span> 
+                <span > { data?.movieDetails?.year } </span> 
               </div>
               <div className='mt-[10px] text-[13px]'>
                 <span className='text-gray-600 inline-flex w-[180px]'> Страна </span> 
@@ -213,7 +202,7 @@ export const MovieDetails: React.FC = () => {
                 <span>{ movieTime }</span>
               </div>
               <div className='mt-[25px] text-[13px]'>
-                { detailMovie?.description } 
+                { data?.movieDetails?.description } 
               </div>
               <span className='w-[200px] bg-orange-500 py-[10px] px-[20px] rounded-2xl text-center mt-[10px] mb-[10px] text-white'>
                 <a href={`https://flicksbar.club/film/${id}/`} target='_blank' rel="noreferrer">Смотреть фильм</a>
@@ -231,20 +220,20 @@ export const MovieDetails: React.FC = () => {
               <div className='mb-[30px]'>
                 <h3 className='text-[24px]'>Сиквелы и приквелы ›</h3>
                 <div className='flex ml=[30px] mt-[15px]'>
-                  {prequelMovies && <RelatedFilms relatedFilms={prequelMovies} />}
+                  {data?.PrequelMovies && <RelatedFilms relatedFilms={data?.PrequelMovies} />}
                 </div>
               </div>
             }
             <div className='mb-[30px]'>
               <h3 className='text-[24px]'>Если вам понравился этот фильм ›</h3>
               <div className='flex ml=[30px] mt-[15px]'>
-              {similarsMovie && <RelatedFilms relatedFilms={similarsMovie.items} />}
+              {data?.SimilarsMovie && <RelatedFilms relatedFilms={data?.SimilarsMovie.items} />}
               </div>
             </div>
             <div className='pb-[25px] max-w-[70%]'>
               <h3 className='text-[24px]'>Знаете ли вы, что…</h3>
               {
-                facts?.items.slice(0, 10).map((fact, index) => <div key={index} dangerouslySetInnerHTML={{ __html: fact.text }} className='border-b-2 mt-[25px] pb-[15px]'></div>)
+                data?.FactsMovie?.items.slice(0, 10).map((fact, index) => <div key={index} dangerouslySetInnerHTML={{ __html: fact.text }} className='border-b-2 mt-[25px] pb-[15px]'></div>)
               }
             </div>
           </div>
--- src/redux/movies.api.ts
+++ src/redux/movies.api.ts
@@ -15,21 +15,20 @@ import {
   CombinedData
 } from '../models/IMovieModels'
 
-const headers = {
-  'X-API-KEY': '03b257a3-99b3-43ff-be90-2f7b5b72e260',
-  'Content-Type': 'application/json',
-}
-
 export const movieApi = createApi({
   reducerPath: 'movieApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://kinopoiskapiunofficial.tech/api'
+    baseUrl: 'https://kinopoiskapiunofficial.tech/api',
+    prepareHeaders: (headers) => {
+      headers.set('X-API-KEY', '03b257a3-99b3-43ff-be90-2f7b5b72e260')
+      headers.set('Content-Type', 'application/json')
+      return headers
+    }
   }),
   endpoints: build => ({
     mainMovies: build.query<ServerMoviesResponse<IMovie>, string>({
       query: () => ({
         url: `/v2.2/films`,
-        headers,
         params: {
           countries: 1,
           genres: 2,
@@ -46,7 +45,6 @@ export const movieApi = createApi({
     moviesSearch: build.query<ServerSearchResponse<ISearchMovie>, string>({
       query: (debounced: string) => ({
         url: `/v2.1/films/search-by-keyword`,
-        headers,
         params: {
           keyword: debounced,
           page: 1
@@ -54,115 +52,60 @@ export const movieApi = createApi({
       })
     }),
 
-    // getCombineDataOnMovie: build.query<CombinedData, string>({
-    //   async queryFn(id: string, _queryApi, _extraOptions, fetchWithBQ) {
+    getCombineDataOnMovie: build.query<CombinedData, string | undefined>({
+      async queryFn(id: string, _queryApi, _extraOptions, fetchWithBQ) {
 
-    //     const baseDetailMovie = await fetchWithBQ(`v2.2/films/${id}`)
-    //     if(baseDetailMovie.error)
-    //       return {error: baseDetailMovie.error as FetchBaseQueryError}
+        const baseDetailMovie = await fetchWithBQ(`v2.2/films/${id}`)
+        if(baseDetailMovie.error)
+          return {error: baseDetailMovie.error as FetchBaseQueryError}
         
-    //     const budgetMovie = await fetchWithBQ(`/v2.2/films/${id}/box_office`)
-    //     if(budgetMovie.error)
-    //       return {error: budgetMovie.error as FetchBaseQueryError}
+        const budgetMovie = await fetchWithBQ(`/v2.2/films/${id}/box_office`)
+        if(budgetMovie.error)
+          return {error: budgetMovie.error as FetchBaseQueryError}
 
-    //     const StaffMovie = await fetchWithBQ(`/v2.2/films/${id}/box_office`)
-    //     if(StaffMovie.error)
-    //       return {error: StaffMovie.error as FetchBaseQueryError}  
+        const StaffMovie = await fetchWithBQ(`/v1/staff?filmId=${id}`)
+        if(StaffMovie.error)
+          return {error: StaffMovie.error as FetchBaseQueryError}  
 
-    //     const DistributionMovie = await fetchWithBQ(`/v2.2/films/${id}/distributions`)
-    //     if(DistributionMovie.error)
-    //       return {error: DistributionMovie.error as FetchBaseQueryError}  
+        const DistributionMovie = await fetchWithBQ(`/v2.2/films/${id}/distributions`)
+        if(DistributionMovie.error)
+          return {error: DistributionMovie.error as FetchBaseQueryError}  
 
-    //     const PrequelMovies = await fetchWithBQ(`v2.1/films/${id}/sequels_and_prequels`)
-    //     if(PrequelMovies.error)
-    //       return {error: PrequelMovies.error as FetchBaseQueryError}  
+        const PrequelMovies = await fetchWithBQ(`v2.1/films/${id}/sequels_and_prequels`)
+        if(PrequelMovies.error)
+          return {data: undefined, error: PrequelMovies.error as FetchBaseQueryError}  
 
-    //     const FactsMovie = await fetchWithBQ(`/v2.2/films/${id}/facts`)
-    //     if(FactsMovie.error)
-    //       return {error: FactsMovie.error as FetchBaseQueryError}  
+        const FactsMovie = await fetchWithBQ(`/v2.2/films/${id}/facts`)
+        if(FactsMovie.error)
+          return {error: FactsMovie.error as FetchBaseQueryError}  
 
-    //     const SimilarsMovie = await fetchWithBQ(`/v2.2/films/${id}/similars`)
-    //     if(SimilarsMovie.error)
-    //       return {error: SimilarsMovie.error as FetchBaseQueryError}  
+        const SimilarsMovie = await fetchWithBQ(`/v2.2/films/${id}/similars`)
+        if(SimilarsMovie.error)
+          return {error: SimilarsMovie.error as FetchBaseQueryError}  
 
-    //     const combinedData: CombinedData = {
-    //       movieDetails: baseDetailMovie.data as IMovieDetail,
-    //       budget: budgetMovie.data as ServerResponse<IBudget>,
-    //       staff: StaffMovie.data as IStaff[],
-    //       distribution: DistributionMovie.data as ServerResponse<IDistribution>,
-    //       FactsMovie: FactsMovie.data as ServerResponse<IFacts>,
-    //       PrequelMovies: PrequelMovies.data as IRelatedFilms[],
-    //       SimilarsMovie: SimilarsMovie.data as ServerResponse<IRelatedFilms>
-    //     }
-    //     return combinedData
-    //   }
-    // }),
-
-    getMovieDetails: build.query<IMovieDetail, string | undefined>({
-      query: (id: string) => ({
-        url: `v2.2/films/${id}`,
-        headers
-      })
-    }),
-
-    getStaffMovie: build.query<IStaff[], string | undefined>({
-      query: (id: string) => ({
-        url: `v1/staff`,
-        headers,
-        params: {
-          filmId: id
+        const combinedData: CombinedData = {
+          movieDetails: baseDetailMovie.data as IMovieDetail,
+          budget: budgetMovie.data as ServerResponse<IBudget>,
+          staff: StaffMovie.data as IStaff[],
+          distribution: DistributionMovie.data as ServerResponse<IDistribution>,
+          FactsMovie: FactsMovie.data as ServerResponse<IFacts>,
+          PrequelMovies: PrequelMovies.data as IRelatedFilms[],
+          SimilarsMovie: SimilarsMovie.data as ServerResponse<IRelatedFilms>
         }
-      })
-    }),
-    getBudgetMovie: build.query<ServerResponse<IBudget>, string | undefined>({
-      query: (id: string) => ({
-        url: `/v2.2/films/${id}/box_office`,
-        headers
-      })
-    }),
-    getDistributionMovie: build.query<ServerResponse<IDistribution>, string | undefined>({
-      query: (id: string) => ({
-        url: `/v2.2/films/${id}/distributions`,
-        headers
-      })
-    }),
-    getPrequelMovies: build.query<IRelatedFilms[], string | undefined>({
-      query: (id: string) => ({
-        url: `v2.1/films/${id}/sequels_and_prequels`,
-        headers
-      })
-    }),
-    getFactsMovie: build.query<ServerResponse<IFacts>, string | undefined>({
-      query: (id: string) => ({
-        url: `/v2.2/films/${id}/facts`,
-        headers
-      })
+        return {data: combinedData}
+      }
     }),
     getDetailsPerson: build.query<IPersonDetail, string | undefined>({
       query: (id: string) => ({
         url: `/v1/staff/${id}`,
-        headers
       })
     }),
-    getSimilarsMovie: build.query<ServerResponse<IRelatedFilms>, string | undefined>({
-      query: (id: string) => ({
-        url: `/v2.2/films/${id}/similars`,
-        headers
-      })
-    })
   })
 })
 
 export const { 
   useMainMoviesQuery, 
   useMoviesSearchQuery, 
-  useGetMovieDetailsQuery, 
-  useGetStaffMovieQuery,
-  useGetBudgetMovieQuery,
-  useGetDistributionMovieQuery,
-  useGetPrequelMoviesQuery,
-  useGetFactsMovieQuery,
   useGetDetailsPersonQuery,
-  useGetSimilarsMovieQuery,
-  // useGetCombineDataOnMovieQuery
+  useGetCombineDataOnMovieQuery
 } = movieApi
